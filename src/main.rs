f991250a5e1570dd8b08000a0b48ce7203b7d200
PabloMansanet <pablomansanet@gmail.com>
2023-11-24 12:41:08 +0900

    First-order differential beamforming

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use arrayvec::ArrayVec;
 use fugit::RateExtU32;
 use panic_reset as _;
 use rp2040_hal::{
-    dma::DMAExt,
     gpio::{FunctionPio0, Pin},
     pac,
     pio::PIOExt,
@@ -22,10 +21,6 @@ mod clocks;
 pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
 const EXTERNAL_CRYSTAL_FREQUENCY_HZ: u32 = 12_000_000;
-
-// For single-buffered DMA transfers, this only seems to work if it holds
-// enough data for 4 sine-wave periods.
-static mut AUDIO_BUF: &mut [u32; 9600] = &mut [0; 9600];
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
@@ -143,74 +138,29 @@ fn main() -> ! {
         (mic_left_right_clock_pin_id, rp2040_hal::pio::PinDir::Output),
         (mic_bit_clock_pin_id, rp2040_hal::pio::PinDir::Output),
     ]);
-    // End I2S input
-
-    // let mut tone_generator = ToneGenerator::new(300, 48_000);
-
-    // // Fill the buffer with data once
-    // unsafe {
-    //     for frame in AUDIO_BUF.chunks_mut(2) {
-    //         let val = tone_generator.next() as u32;
-    //         frame[0] = val;
-    //         frame[1] = val;
-    //     }
-    // }
-
-    // let mut dma = pac.DMA.split(&mut pac.RESETS);
 
     let sm_group = dac_sm.with(mic_sm);
     sm_group.start();
 
-    let mut buffer: ArrayVec<u32, 2> = ArrayVec::new();
+    let mut back_mic_buffer = SampleDelay::<u32, 4>::new();
+    let mut buffer = ArrayVec::<u32, 2>::new();
 
     loop {
         if let Some(val) = fifo_rx.read() {
-            buffer.push(val);
+            match buffer.len() {
+                0 => buffer.push(val),
+                1 => buffer.push(back_mic_buffer.process(val)),
+                _ => (),
+            }
         }
 
         if buffer.len() == 2 {
-            let val = buffer.pop().unwrap() + buffer.pop().unwrap();
+            let front = buffer.pop().unwrap();
+            let back = buffer.pop().unwrap();
+            let val = front.wrapping_sub(back);
             fifo_tx.write(val);
             fifo_tx.write(val);
         }
-
-        // let mut transfer_config =
-        //     rp2040_hal::dma::single_buffer::Config::new(dma.ch0, fifo_rx, fifo_tx);
-        // transfer_config.pace(rp2040_hal::dma::Pace::PreferSink);
-
-        // let transfer = transfer_config.start();
-
-        // // Here is where we should fill the buffer with more data while the PIO is outputting audio data.
-        // // TODO - Use double-buffered DMA
-
-        // let (ch0, old_fifo_rx, old_fifo_tx) = transfer.wait();
-        // dma.ch0 = ch0;
-        // fifo_tx = old_fifo_tx;
-        // fifo_rx = old_fifo_rx;
-    }
-}
-
-struct ToneGenerator {
-    delta: f64,
-    current: f64,
-}
-
-impl ToneGenerator {
-    pub fn new(frequency_hz: u32, sample_rate: u32) -> Self {
-        let delta = (frequency_hz as f64 * core::f64::consts::TAU) / sample_rate as f64;
-
-        Self { delta, current: 0.0 }
-    }
-
-    pub fn next(&mut self) -> i32 {
-        let val = libm::sin(self.current);
-        self.current += self.delta;
-
-        if self.current > core::f64::consts::TAU {
-            self.current -= core::f64::consts::TAU;
-        }
-
-        libm::floor(val * 0.5 * i32::MAX as f64) as i32
     }
 }
 
@@ -219,9 +169,9 @@ struct SampleDelay<T, const N: usize> {
     position: usize,
 }
 
-impl<T: Copy, const N: usize> SampleDelay<T, N> {
+impl<T: Copy + Default, const N: usize> SampleDelay<T, N> {
     pub fn new() -> Self {
-        Self { buffer: ArrayVec::new(), position: 0 }
+        Self { buffer: ArrayVec::from([T::default(); N]), position: 0 }
     }
 
     pub fn process(&mut self, input_sample: T) -> T {
